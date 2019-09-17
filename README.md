03e1280291036aa775be6ba0598ab3e016f3be88
李江 <lijiang@xiaoshouyi.com>
2019-09-17 18:42:26 +0800

    2019-09-17提交

--- README.md
+++ README.md
@@ -50,7 +50,98 @@ while(index <= 10):
     index += 1
 ```
 
+######while else
+
+```python
+a = 1
+while a < 4:
+    print(a)
+    a+=1
+else:
+    print('a 大于等于 4')
+```
+
+###### 死循环
+
+```python
+# 使用 CTRL+C 结束程序
+while True:
+    print('死循环')
+```
+
+例1：
+
+```python
+# 求 1 + 2 + 3 + ... + 100
+sum = 1
+result = 0
+while sum <= 100:
+    result += sum
+    sum+=1
+print('result = ', result)
+```
+
+##### for
+
+```python
+for var in sequence:
+    # do something
+else:
+    # do something else
+```
+
+例1:
+
+```python
+list = ['a', 'b', 'c', 'd']
+for li in list:
+    print(li)
+else:
+    print("没有循环数据!")
+```
+
+##### break
+
+例1: 猜数字
+
+```python
+a = 80
+while True:
+    number = int(input("请猜测:"))
+    if number > a:
+        print('大了')
+    elif number < a:
+        print('小了')
+    else:
+        print('中了')
+        break
+```
+
+##### continue
+
+例2: 打印偶数
+
+```python
+list = [1, 2, 3, 5, 6, 8, 10]
+for li in list:
+    if li%2!=0:
+        continue
+    print(li)
+```
+
+##### pass
+
+Python pass是空语句，是为了保持程序结构的完整性。pass 不做任何事情，一般用做占位语句，如下实例
+
+```python
+
+list = [1, 2, 3, 5, 6, 8, 10]
+for li in list:
+    if li%2!=0:
+        continue
+    pass
+    print(li)
+```
 
 
-###### for
 
--- ifDemo.py
+++ ifDemo.py
@@ -1,18 +1,88 @@
 #!/usr/bin/python3
 
 
-score = float(input("请输入考试成绩:"))
-if score < 60:
-    print('差：不及格')
-elif score <= 80:
-    print('良：考的不错')
-elif score <= 100:
-    print('优秀：再接再厉')
-else:
-    print('无法识别')
+# score = float(input("请输入考试成绩:"))
+# if score < 60:
+#     print('差：不及格')
+# elif score <= 80:
+#     print('良：考的不错')
+# elif score <= 100:
+#     print('优秀：再接再厉')
+# else:
+#     print('无法识别')
 
 
-index = 0
-while(index <= 10):
-    print('index=', index)
-    index += 1
+# index = 0
+# while index <= 10:
+#     print('index=', index)
+#     index += 1
+
+
+# a = 1
+# while a < 4:
+#     print(a)
+#     a+=1
+# else:
+#     print('a 大于等于 4')
+
+# while True:
+#     print('死循环')
+
+# 求 1 + 2 + 3 + ... + 100
+# sum = 1
+# result = 0
+# while sum <= 100:
+#     result += sum
+#     sum+=1
+# print('result = ', result)
+
+# for var in sequence:
+#     # do something
+# else:
+#     # do something else
+
+# list = ['a', 'b', 'c', 'd']
+# for li in list:
+#     print(li)
+# else:
+#     print('遍历结束')
+
+# number = int(input("请猜测:"))
+
+# a = 80
+# while number != a:
+#     if number > a:
+#         print('大了')
+#     elif number < a:
+#         print('小了')
+#     number = int(input("请猜测:"))
+
+# print('猜中了')
+
+
+
+# a = 80
+# while True:
+#     number = int(input("请猜测:"))
+#     if number > a:
+#         print('大了')
+#     elif number < a:
+#         print('小了')
+#     else:
+#         print('中了')
+#         break
+
+
+# list = [1, 2, 3, 5, 6, 8, 10]
+# for li in list:
+#     if li%2!=0:
+#         continue
+#     print(li)
+
+
+# list = [1, 2, 3, 5, 6, 8, 10]
+# for li in list:
+#     if li%2!=0:
+#         continue
+#     pass
+#     print(li)
